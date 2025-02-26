8775983680d27c9471d2ae2b85ea90f62227d036
A K Giri <ashishkumargiri13@gmail.com>
2025-02-26 16:24:26 +0530

    add new image

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,7 +1,7 @@
 import "./intro.css";        //normal css
 import React from 'react'
 import { Link } from 'react-scroll';   
-import bg from "../../assets/image44.png"
+import bg from "../../assets/akgiri10.png"
 import btnImg from "../../assets/hireme.png"
 
 const Intro = () => {
@@ -10,8 +10,8 @@ return (
         <div className="introContent">
             <span className="hello">Hello</span>
             <span className="introText"> I'm <span className="introName">Ashish,<br/></span>Website Devloper</span>
-            <p className="introPara">\\Transforming ideas into seamless, user-centric web experiences<br></br>
-            \\explore my portfolio to see how I bring creativity and functionality to life.</p>
+            <p className="introPara">Transforming ideas into seamless, user-centric web experiences<br></br>
+            explore my portfolio to see how I bring creativity and functionality to life.</p>
             <Link><button className="btn"><img src={btnImg} alt="hire me" className="btnImg"/> Hire Me</button></Link>
         </div>
         <img src={bg} alt="my image" className="bg" />
