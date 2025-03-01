b856fa05aebd45191a4abbe35ef641622f12121e
A K Giri <ashishkumargiri13@gmail.com>
2025-03-01 20:38:05 +0530

    add emailJs ......
    i add the email js for directly contacting me , THank you

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./contact.css";
-import Facebook from "../../assets/facebook_icon1.png"
+import Facebook from "../../assets/facebook_icon1.png" //not added
 import Instagram from "../../assets/instagram2.png"
 import Linkedin from "../../assets/linkedin.png"
 import GitHub from "../../assets/github22.png"
 import Telegram from "../../assets/telegram.png"
-import Twiter from "../../assets/twitter1.png"
-import YouTube from "../../assets/youtube33.png"
+import Twiter from "../../assets/twitter1.png"  //not added
+import YouTube from "../../assets/youtube33.png" //not added
+import emailjs from '@emailjs/browser';   //install emailJs =: npm install --save @emailjs/browser
 
 
 function Contact() {
+// auto email send when submit
+  const form = useRef();    // for emailJs ----
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_AkGiri10', 'template_wlmohps', form.current, {   //service id , template id ,from the emailJs website
+        publicKey: '81QqN3g0dWaDM0eAm',
+      })
+      .then((result) => {
+          console.log(result.text , "SUCESS..!");
+          e.target.reset();
+          alert("Email Sent..👌,Thank you for Contact me 😁, Stay connected I will connect you soon.");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          alert("Email Failed to Send !!😢");
+        },
+      );
+  };
+
+// 
+
+
   return (
     <section id="contactPage">
         <div className="contact">
           <h1 className="contactPageTitle">Contact Me</h1>
           <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
-          <form action="" className="contactForm">
-            <input type="text" className="name" placeholder='Your Name' />
-            <input type="email" className="email" placeholder='Your Email' />
-            <textarea name="message" rows="5" placeholder='Give your message here...' className="msg"></textarea>
-            <button className="submitBtn" type='submit' value="send" >Submit</button>
+
+          <form action="" className="contactForm" ref={form} onSubmit={sendEmail} >
+            <input type="text" className="name" placeholder='Your Name' name="your_name" />
+            <input type="email" className="email" placeholder='Your Email' name="your_email" />
+            <textarea rows="5" placeholder='Give your message here...' className="msg" name="message" ></textarea>
+            <button className="submitBtn" type='submit' value="send" >SEND</button>
             <div className="links">
               
               <img src={Instagram} alt="Instagram" className="link" />
-              <img src={Twiter} alt="Twiter" className="link" />
               <img src={Linkedin} alt="Linkedin" className="link" />
               <img src={GitHub} alt="GitHub" className="link" />
               <img src={Telegram} alt="Telegram" className="link" />
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import style from'./navbar.module.css';    //module css
-import logo from '../../assets/logo2.png';
+import logo from '../../assets/letter-gr-a.png';
 import { Link } from 'react-scroll';  //npm i react-scroll
 import contactImg from '/src/assets/contact.png';
+import Menu from "../../assets/main-menu.png";
 
 
 const Navbar =()=>{
+
+    //for reminding and scroll
+    const contactMe = () => {
+        const contactPage = document.getElementById('contactPage');
+        const offset = 100; // Adjust this value based on your needs (e.g., height of your fixed header)
+    
+        // Calculate the position of the element minus the offset
+        const elementPosition = contactPage.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition - offset;
+    
+        // Scroll to the adjusted position
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    };
+
+
+
+    //react hook for mobile view
+    const [showMenu, setShowMenu] = useState(false);
+
+
+
+
+    //offset={-100} ------------use  for perfect view , no override page
     return(
         <nav className={style.navbar}>
             <img src={logo} alt="logo" className={style.logo} />
 
-            <div className="desktopMenu">
-                <Link className={style.desktopMenuListItem}>Home</Link>
-                <Link className={style.desktopMenuListItem}>About</Link>
-                <Link className={style.desktopMenuListItem}>Portfoleo</Link>
-                <Link className={style.desktopMenuListItem}>Career</Link>
+            <div className={style.desktopMenu}>
+                <Link activeClass={style.active} to="intro" spy={true} smooth={true} offset={-100} duration={500} className={style.desktopMenuListItem}>Home</Link>
+                <Link activeClass={style.active} to="skills" spy={true} smooth={true} offset={-50} duration={500} className={style.desktopMenuListItem}>About</Link>
+                <Link activeClass={style.active} to="works" spy={true} smooth={true} offset={-50} duration={500} className={style.desktopMenuListItem}>Portfoleo</Link>
+                <Link activeClass={style.active} to="career" spy={true} smooth={true} offset={-50} duration={500} className={style.desktopMenuListItem}>Career</Link>
             </div>
-
-            <button className={style.desktopMenuBtn}>
+            <button className={style.desktopMenuBtn} onClick={contactMe}>
                 <img src={contactImg} alt="" className={style.desktopMenuImg} />Contact Me
             </button>
+
+
+            <img src={Menu} alt="Menu" className={style.mobMenu} onClick={()=>setShowMenu(!showMenu)} />
+            <div className={style.mobnavMenu} style={{display: showMenu? 'flex':'none'}}>
+                <Link activeClass={style.active} to="intro" spy={true} smooth={true} offset={-100} duration={500} className={style.mobMenuListItem} onClick={()=>setShowMenu(false)} >Home</Link>
+                <Link activeClass={style.active} to="skills" spy={true} smooth={true} offset={-50} duration={500} className={style.mobMenuListItem} onClick={()=>setShowMenu(false)} >About</Link>
+                <Link activeClass={style.active} to="works" spy={true} smooth={true} offset={-50} duration={500} className={style.mobMenuListItem} onClick={()=>setShowMenu(false)} >Portfoleo</Link>
+                <Link activeClass={style.active} to="career" spy={true} smooth={true} offset={-50} duration={500} className={style.mobMenuListItem} onClick={()=>setShowMenu(false)} >Career</Link>
+                <Link activeClass={style.active} to="contact" spy={true} smooth={true} offset={-50} duration={500} className={style.mobMenuListItem} onClick={()=>setShowMenu(false)} >Contact Me</Link>
+            </div>
         </nav>
     )
 }
