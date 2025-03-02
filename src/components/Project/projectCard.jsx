4149dfaba81b02b3b4546071f046bd63cf20d73e
A K Giri <ashishkumargiri13@gmail.com>
2025-03-02 12:27:59 +0530

    add project 1

--- src/components/Project/projectCard.jsx
+++ src/components/Project/projectCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./projectCard.css"
-import portfolio1 from "../../assets/portfolio-1.png";
+import portfolio1 from "../../assets/project11.png";
 
 
 const ProjectCard = () => {
@@ -25,16 +25,18 @@ const ProjectCard = () => {
                 <a href="http://" target="_blank" rel="noopener noreferrer"><button className="button">Demo</button></a>
             </div>
             <div className="project">
-                <h3 className="proName">Deemo project</h3>
-                <img src={portfolio1} alt="" className="proImg" />
+                <h3 className="proName">My Portfolio 2</h3>
+                <img src={portfolio1} alt="my portfolio project" className="proImg" />
                 <li className="list">
-                    <ul>java</ul>
-                    <ul>react</ul>
-                    <ul>Spring</ul>
+                    <ul>reactJS</ul>
+                    <ul>HTML</ul>
+                    <ul>CSS</ul>
+                    <ul>emailJS</ul>
+                    <ul>JavaScript</ul>
                 </li>
                 <p className="proDetail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, culpa.</p>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><button className="button">GitHub</button></a>
-                <a href="http://" target="_blank" rel="noopener noreferrer"><button className="button">Demo</button></a>
+                <a href="https://github.com/Ashish-1013/A.K.Giri" target="_blank" rel="noopener noreferrer"><button className="button">GitHub</button></a>
+                <a href="https://ashish-1013.github.io/A.K.Giri/" target="_blank" rel="noopener noreferrer"><button className="button">Demo</button></a>
             </div>
             <div className="project">
                 <h3 className="proName">Deemo project</h3>
